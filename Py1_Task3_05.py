3ddef026b1827adf1e5da566dda88967eaff7579
pmonda <42980918+pmonda@users.noreply.github.com>
2021-09-09 08:42:11 -0400

    Create Py1_Task3_05.py

---
+++ Py1_Task3_05.py
@@ -0,0 +1,35 @@
+"""
+===============================================================================
+ENGR 13300 Fall 2021
+
+Program Description
+    Replace this line with a description of our program.
+
+Assignment Information
+    Assignment:     e.g. Ind HW4 - PY 1
+    Author:         Name, login@purdue.edu
+    Team ID:        LC# - ## (e.g. LC1 - 01; for section LC1, team 01)
+
+Contributor:    Name, login@purdue [repeat for each]
+    My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor here as well.
+    
+ACADEMIC INTEGRITY STATEMENT
+I have not used source code obtained from any other unauthorized
+source, either modified or unmodified. Neither have I provided
+access to my code to another. The project I am submitting
+is my own original work.
+===============================================================================
+"""
+def main():
+  
+  
+if __name__ == '__main__':
+  main()
